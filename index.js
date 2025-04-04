7c49c18c499775756feb29cacb49992658d7366f
Abdou De <derrarabdelillah55@gmail.com>
2025-04-04 16:27:19 +0100

    the project was completed

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-$(document).ready(function () {
-    var multipleCardCarousel = document.querySelector(
-    "#carouselExampleControls"
-  );
-  if (window.matchMedia("(min-width: 576px)").matches) {
-    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-      interval: false,
-      wrap: false
-    });
-    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-    var cardWidth = $(".carousel-item").width();
-    var scrollPosition = 0;
-    $("#carouselExampleControls .carousel-control-next").on("click", function () {
-      if (scrollPosition < carouselWidth - cardWidth * 3) {
-        scrollPosition += cardWidth;
-        $("#carouselExampleControls .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
-    });
-    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
-      if (scrollPosition > 1) {
-        scrollPosition -= cardWidth;
-        $("#carouselExampleControls .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
-    });
-  } else {
-    $(multipleCardCarousel).addClass("slide");
-  }
-});
+let topButton = document.querySelector(".top");
+
+window.onscroll = function() {
+    if( scrollY >= 500 ) {
+        topButton.classList.add("active");
+    } else {
+        topButton.classList.remove("active");
+    }
+}
+
+topButton.addEventListener("click", function() {
+    scrollTo({
+        top: 0,
+        behavio: "smooth"
+    })
+})
