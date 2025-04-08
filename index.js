fe7547f9d8e4a1d2d724862cd50234f0100d7cdd
Abdou De <derrarabdelillah55@gmail.com>
2025-04-08 14:45:38 +0100

    i just added scroller progress feature

--- index.js
+++ index.js
@@ -53,3 +53,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+// Scroller Progress
+
+let scroller = document.querySelector(".scroller");
+
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+    let scrollTop = document.documentElement.scrollTop;
+    scroller.style.width = `${scrollTop / height * 100}%`;
+})
